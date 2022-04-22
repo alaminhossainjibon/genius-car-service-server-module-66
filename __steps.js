03991211d51a3ef46e81c8f920c359589a7c9309
alaminhossainjibon <mdalamin7622@gmail.com>
2022-04-22 17:45:28 +0600

    genius car service server

---
+++ __steps.js
@@ -0,0 +1,24 @@
+/**
+ * -------------------------------
+ * first setup
+ * --------------------------------
+ * 1. create a folder mkdir folder name
+ * 2. npm init -y
+ * 3. create index.js
+ * 4. npm install express cors mongodb dotenv
+ * 5. create a .env file
+ * 6. package.json ==> "scripts": ==> 
+ * 6.1. "start": "node index.js",
+ * 6.2. "start-dev": "nodemon index.js",
+ * 7. npm run start-dev
+ * 
+ * --------------------------------
+ * mongodb in data load
+ * --------------------------------
+ * 1. go to mongodb atlas
+ * 2. create a database
+ * 3. go to insert document past in data
+ * 4. warks start go to node mongodb CRUD ==> fundamental
+ * 5 .usage example => find operation => find multiple
+ * 
+ */
