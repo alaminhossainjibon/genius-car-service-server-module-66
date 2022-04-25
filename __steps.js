36a476ffadae52261229f9c1edd2ad67b33771dd
alaminhossainjibon <mdalamin7622@gmail.com>
2022-04-25 23:27:20 +0600

    added hero api

--- __steps.js
+++ __steps.js
@@ -51,6 +51,20 @@
  * 1. heroku create
  * 2. make sure : git add . commit. git push
  * 3. git push heroku main
- * 4. 
+ * 4. gp to heroku Dashboard ==> current project ==> settings ==> reveal config vars
+ * 5. copy paste config vars from in .env file
+ * 6. Make sure have whitelisted all ip address to access mongodb
+ * 
+ * ---------------------------------
+ * UPDATE SERVER WITH NEW CHANGES
+ * ------------------------------
+ * 1. Make changes
+ * 2. make sure : git add . commit. git push
+ * 3. git push heroku main
+ * 
+ * ------------------------------------
+ * 
+ * -----------------------------------
+ * 1. 
  * 
  */
