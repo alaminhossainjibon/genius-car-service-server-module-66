592d867908f86bea3c14d6805385cc9ad787f93b
alaminhossainjibon <mdalamin7622@gmail.com>
2022-04-25 22:57:47 +0600

    adedd __steps

--- __steps.js
+++ __steps.js
@@ -33,3 +33,24 @@
  * 4.  
  * 
  */
+
+/**
+ * ----------------------------
+ * heroku setup
+ * ------------------------------
+ * ONE TIME FOR MY COMPUTE
+ * ----------------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * 
+ * -------------------------------------
+ * FOR EACH PROJECT ONE TIME
+ * --------------------------------------
+ * 1. heroku create
+ * 2. make sure : git add . commit. git push
+ * 3. git push heroku main
+ * 4. 
+ * 
+ */
