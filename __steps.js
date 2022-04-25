810f06c1b521333d0652a201b5cf131860f496d4
alaminhossainjibon <mdalamin7622@gmail.com>
2022-04-25 17:35:19 +0600

    json web token cteate my genius car server

--- __steps.js
+++ __steps.js
@@ -21,4 +21,15 @@
  * 4. warks start go to node mongodb CRUD ==> fundamental
  * 5 .usage example => find operation => find multiple
  * 
+ * ------------------------------
+ * install jsonwebtoken
+ * ----------------------------
+ * 1. cmd: npm i jsonwebtoken
+ * 2. use jwt website
+ * 3. secret random key creat:
+ * 3.1. go to node
+ * 3.2. typing:  require('crypto').randomBytes(64).toString('hex') 
+ * 3.3. copy key and pest in .env file
+ * 4.  
+ * 
  */
